60b9e32510a345bd2bc72514e9fed6da63dfd349
phao90016-jpg <phao90016@gmail.com>
2026-08-22 11:21:24 +0800

    完善process.rs文件，避免死锁问题

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -66,6 +66,7 @@ pub fn sys_fork() -> isize {
 pub fn sys_exec(path: *const u8, mut args: *const usize) -> isize {
     let token = current_user_token();
     let path = translated_str(token, path);
+
     let mut args_vec: Vec<String> = Vec::new();
     loop {
         let arg_str_ptr = *translated_ref(token, args);
@@ -77,12 +78,14 @@ pub fn sys_exec(path: *const u8, mut args: *const usize) -> isize {
             args = args.add(1);
         }
     }
+
     if let Some(app_inode) = open_file(path.as_str(), OpenFlags::RDONLY) {
         let all_data = app_inode.read_all();
         let process = current_process();
         let argc = args_vec.len();
+
         process.exec(all_data.as_slice(), args_vec);
-        // return argc because cx.x[10] will be covered with it later
+
         argc as isize
     } else {
         -1
